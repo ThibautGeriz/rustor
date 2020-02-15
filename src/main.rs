7e15f6fea497480fd2d6149b83ab7a1f69171bbe
ThibautGeriz <thibaut@snooper-app.com>
2020-02-15 14:15:26 +1100

    Use alternate screen for app

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 extern crate termion;
 
-use std::cmp;
 use std::env;
 use std::fs::File;
+use std::io::Error;
 use std::io::{self, BufRead};
 use std::io::{stdin, stdout, Write};
-use std::io::Error;
 use std::path::Path;
-use std::process::exit;
 
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use termion::screen::*;
 
 use cursor::*;
 use window::*;
 
-mod window;
 mod cursor;
+mod window;
 
-fn backspace_remove_characters_in_line(y_position_in_file: usize, cursor: &mut CursorPosition, lines: &mut Vec<String>) {
+fn backspace_remove_characters_in_line(
+    y_position_in_file: usize,
+    cursor: &mut CursorPosition,
+    lines: &mut Vec<String>,
+) {
     let current_line = &mut lines[y_position_in_file - 1];
     cursor.move_left();
     current_line.remove(cursor.x as usize - 1);
@@ -28,36 +31,31 @@ fn backspace_remove_characters_in_line(y_position_in_file: usize, cursor: &mut C
 fn backspace_remove_line_break_when_not_on_first_line(
     y_position_in_file: usize,
     cursor: &mut CursorPosition,
-    lines: &mut Vec<String>) {
+    lines: &mut Vec<String>,
+) {
     let cursor_x = cursor.x;
     cursor.move_up(&lines);
     cursor.move_to_end_of_line(&lines);
     if cursor_x == 1 {
-        move_content_from_current_line_to_previous_line(y_position_in_file, cursor, lines);
+        move_content_from_current_line_to_previous_line(y_position_in_file, lines);
     }
     lines.remove(y_position_in_file - 1);
 }
 
-fn move_content_from_current_line_to_previous_line(y_position_in_file: usize,
-                                                   cursor: &mut CursorPosition,
-                                                   lines: &mut Vec<String>) {
+fn move_content_from_current_line_to_previous_line(
+    y_position_in_file: usize,
+    lines: &mut Vec<String>,
+) {
     let current_line = lines[y_position_in_file - 1].clone();
     let previous_line = &mut lines[y_position_in_file - 2];
     previous_line.push_str(&current_line);
 }
 
-fn backspace_remove_first_displayed_line(y_position_in_file: usize,
-                                         cursor: &mut CursorPosition,
-                                         lines: &mut Vec<String>) {
-    let nb_char_in_previous_line = lines[y_position_in_file - 2].len() as u16;
-    cursor.x = nb_char_in_previous_line + 1;
-    lines.remove(y_position_in_file - 1);
-}
-
-fn handle_key_press(key: Result<Key, Error>,
-                    lines: &mut Vec<String>,
-                    cursor: &mut CursorPosition) {
-    let nb_lines = lines.len() as u16;
+fn handle_key_press(
+    key: Result<Key, Error>,
+    lines: &mut Vec<String>,
+    cursor: &mut CursorPosition,
+) -> bool {
     let (_, terminal_height) = termion::terminal_size().unwrap();
     let y_position_in_file = cursor.get_y_position_in_file() as usize;
 
@@ -87,7 +85,11 @@ fn handle_key_press(key: Result<Key, Error>,
             if cursor.x != 1 {
                 backspace_remove_characters_in_line(y_position_in_file, cursor, lines);
             } else if cursor.y > 1 {
-                backspace_remove_line_break_when_not_on_first_line(y_position_in_file, cursor, lines);
+                backspace_remove_line_break_when_not_on_first_line(
+                    y_position_in_file,
+                    cursor,
+                    lines,
+                );
                 if cursor.y_offset > 0 && lines.len() as u16 - cursor.y_offset < terminal_height {
                     cursor.y_offset = cursor.y_offset - 1;
                 }
@@ -109,14 +111,17 @@ fn handle_key_press(key: Result<Key, Error>,
         Key::Down => {
             cursor.move_down(lines, terminal_height);
         }
-        Key::Esc => exit(1),
+        Key::Esc => {
+            return false;
+        }
         _ => (),
     }
+    return true;
 }
 
 fn read_lines<P>(file_name: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where
-        P: AsRef<Path>,
+where
+    P: AsRef<Path>,
 {
     let file = File::open(file_name)?;
     Ok(io::BufReader::new(file).lines())
@@ -145,15 +150,18 @@ fn check_arguments(args: &Vec<String>) {
     }
 }
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     check_arguments(&args);
 
     let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
 
-    let mut cursor = CursorPosition { x: 1, y: 1, y_offset: 0 };
+    let mut cursor = CursorPosition {
+        x: 1,
+        y: 1,
+        y_offset: 0,
+    };
 
     print_first_line(&mut stdout);
 
@@ -164,7 +172,10 @@ fn main() {
 
     for c in stdin.keys() {
         print_first_line(&mut stdout);
-        handle_key_press(c, &mut lines, &mut cursor);
+        let should_continue = handle_key_press(c, &mut lines, &mut cursor);
+        if !should_continue {
+            break;
+        }
         print_text(&mut stdout, &lines, &cursor);
         stdout.flush().unwrap();
     }
@@ -181,7 +192,11 @@ mod tests {
         // Given
         let key = Ok(Key::Char('t'));
         let mut lines: Vec<String> = vec![String::new()];
-        let mut cursor = CursorPosition { x: 1, y: 1, y_offset: 0 };
+        let mut cursor = CursorPosition {
+            x: 1,
+            y: 1,
+            y_offset: 0,
+        };
 
         // When
         handle_key_press(key, &mut lines, &mut cursor);
