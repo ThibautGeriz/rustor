10744a2352bf1462bcb3b21cae1cd2041e98afcc
nicolas <nicolas065@gmail.com>
2020-04-05 18:56:06 +1000

    WIP remove in piece table

--- src/piece_table.rs
+++ src/piece_table.rs
@@ -39,22 +39,32 @@ impl PieceTable {
         let mut text = String::from("");
         for node in &self.nodes {
             let start = node.start as usize;
-            let stop = start + node.length - 1;
+            let stop = start + node.length;
+
             match node.node_type {
                 ADDED => text.push_str(&self.added[start..stop]),
-                // ORIGINAL => text.push_str(&self.original[start..stop]),
-                ORIGINAL => text.push_str(&self.original),
+                ORIGINAL => text.push_str(&self.original[start..stop]),
             }
         }
+
         text
     }
 
     fn push(&mut self, text: String) {
-        self.added.push_str(&text);
         let new_node = Node {
             node_type: ADDED,
             start: self.added.len() as u32,
             length: text.len(),
+        };
+        self.nodes.push(new_node);
+        self.added.push_str(&text);
+    }
+
+    fn remove(&mut self, start_index: u32, length: usize) {
+        let new_node = Node {
+            node_type: ADDED,
+            start: start_index + 1,
+            length: 2,
         };
         self.nodes.push(new_node);
     }
@@ -97,37 +107,51 @@ mod tests {
         assert_eq!(text, String::from("This is a text"))
     }
 
-    // #[test]
-    // fn push_should_add_text_at_end_of_line() {
-    //     // Given
-    //     let input = String::from("This is a text");
-    //     let mut piece_table = PieceTable::new(input);
-    //     let push_str = String::from(".");
+    #[test]
+    fn push_should_add_text_at_end_of_line() {
+        // Given
+        let input = String::from("This is a text");
+        let mut piece_table = PieceTable::new(input);
+        let push_str = String::from(".");
 
-    //     // When
-    //     piece_table.push(push_str);
+        // When
+        piece_table.push(push_str);
 
-    //     // Then
-    //     let text = piece_table.get_text();
-    //     assert_eq!(text, String::from("This is a text."))
-    // }
+        // Then
+        let text = piece_table.get_text();
+        assert_eq!(text, String::from("This is a text."))
+    }
 
-    // #[test]
-    // fn push_should_add_text_at_end_of_line_2() {
-    //     // Given
-    //     let input = String::from("This is a text");
-    //     let mut piece_table = PieceTable::new(input);
-    //     let push_str = String::from(".");
-    //     let push_str2 = String::from("..");
+    #[test]
+    fn push_should_add_text_at_end_of_line_2() {
+        // Given
+        let input = String::from("This is a text");
+        let mut piece_table = PieceTable::new(input);
+        let push_str = String::from(".");
+        let push_str2 = String::from("..");
 
-    //     // When
-    //     piece_table.push(push_str);
-    //     piece_table.push(push_str2);
+        // When
+        piece_table.push(push_str);
+        piece_table.push(push_str2);
 
-    //     // Then
-    //     let text = piece_table.get_text();
-    //     assert_eq!(text, String::from("This is a text..."))
-    // }
+        // Then
+        let text = piece_table.get_text();
+        assert_eq!(text, String::from("This is a text..."))
+    }
+
+    #[test]
+    fn remove_when_index_is_length_of_text_should_remove_characters_at_the_end() {
+        // Given
+        let input = String::from("This is a text...");
+        let mut piece_table = PieceTable::new(input);
+
+        // When
+        piece_table.remove(14, 2);
+
+        // Then
+        let text = piece_table.get_text();
+        assert_eq!(text, String::from("This is a text."));
+    }
 
     //    #[test]
     //    fn insert_should_add_text_in_the_content() {
