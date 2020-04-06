6ff0418c31ec4f7fb10b92847ce7d93f700a5763
ThibautGeriz <thibaut@snooper-app.com>
2020-04-06 11:30:52 +0200

    WIP

--- src/piece_table.rs
+++ src/piece_table.rs
@@ -50,7 +50,7 @@ impl PieceTable {
         text
     }
 
-    fn push(&mut self, text: String) {
+    fn push(mut self, text: String) -> PieceTable {
         let new_node = Node {
             node_type: ADDED,
             start: self.added.len() as u32,
@@ -58,25 +58,57 @@ impl PieceTable {
         };
         self.nodes.push(new_node);
         self.added.push_str(&text);
+        self
     }
 
-    fn remove(&mut self, start_index: u32, length: usize) {
+    fn remove(mut self, start_index: u32, length: usize) -> PieceTable {
         let remove_start_index = start_index as usize;
         let remove_stop_index = remove_start_index + length;
-        println!("{:?}", &self.nodes.iter())
-        // self.nodes = &self
-        //     .nodes
-        //     .into_iter()
-        //     .map(|node| {
-        //         let node_start_index = node.start as usize;
-        //         let node_stop_index = node_start_index + node.length;
-        //         if remove_start_index > node_stop_index || remove_stop_index < node_stop_index {
-        //             return node;
-        //         } else {
-        //             return node;
-        //         }
-        //     })
-        //     .collect::<Vec<Node>>()
+        self.nodes = self
+            .nodes
+            .into_iter()
+            .flat_map(|node| {
+                let node_start_index = node.start as usize;
+                let node_stop_index = node_start_index + node.length;
+                if node_start_index < remove_start_index && node_stop_index > remove_stop_index {
+                    let start_diff = remove_stop_index - node_start_index;
+                    return vec![
+                        Node {
+                            node_type: node.node_type,
+                            start: node_start_index as u32,
+                            length: node.length - start_diff - 1, // not sure about this - 1
+                        },
+                        Node {
+                            node_type: node.node_type,
+                            start: remove_stop_index as u32,
+                            length: node.length - node_stop_index + remove_start_index + 1, // not sure about this + 1
+                        },
+                    ];
+                } else if node_start_index >= remove_start_index
+                    && node_stop_index <= remove_stop_index
+                {
+                    return vec![];
+                } else if node_start_index < remove_stop_index
+                    && remove_start_index <= node_start_index
+                {
+                    let start_diff = remove_stop_index - node_start_index;
+                    return vec![Node {
+                        node_type: node.node_type,
+                        start: node.start + start_diff as u32,
+                        length: node.length - start_diff,
+                    }];
+                } else if node_stop_index > remove_start_index {
+                    return vec![Node {
+                        node_type: node.node_type,
+                        start: node.start,
+                        length: node.length - node_stop_index + remove_start_index,
+                    }];
+                } else {
+                    return vec![node];
+                }
+            })
+            .collect();
+        self
     }
 
     //    fn insert(&mut self, index: u32, text: String) {
@@ -110,7 +142,7 @@ mod tests {
         let input = String::from("This is a text");
 
         // When
-        let mut piece_table = PieceTable::new(input);
+        let piece_table = PieceTable::new(input);
 
         // Then
         let text = piece_table.get_text();
@@ -125,7 +157,7 @@ mod tests {
         let push_str = String::from(".");
 
         // When
-        piece_table.push(push_str);
+        piece_table = piece_table.push(push_str);
 
         // Then
         let text = piece_table.get_text();
@@ -141,8 +173,8 @@ mod tests {
         let push_str2 = String::from("..");
 
         // When
-        piece_table.push(push_str);
-        piece_table.push(push_str2);
+        piece_table = piece_table.push(push_str);
+        piece_table = piece_table.push(push_str2);
 
         // Then
         let text = piece_table.get_text();
@@ -150,13 +182,55 @@ mod tests {
     }
 
     #[test]
-    fn remove_when_index_is_length_of_text_should_remove_characters_at_the_end() {
+    fn remove_when_index_is_length_of_text_should_remove_characters_at_the_end_of_piece() {
         // Given
         let input = String::from("This is a text...");
         let mut piece_table = PieceTable::new(input);
 
         // When
-        piece_table.remove(14, 2);
+        piece_table = piece_table.remove(15, 2);
+
+        // Then
+        let text = piece_table.get_text();
+        assert_eq!(text, String::from("This is a text."));
+    }
+
+    #[test]
+    fn remove_when_index_is_length_of_text_should_remove_characters_at_the_beginning_of_piece() {
+        // Given
+        let input = String::from("xxThis is a text.");
+        let mut piece_table = PieceTable::new(input);
+
+        // When
+        piece_table = piece_table.remove(0, 2);
+
+        // Then
+        let text = piece_table.get_text();
+        assert_eq!(text, String::from("This is a text."));
+    }
+
+    #[test]
+    fn remove_when_index_is_length_of_text_should_remove_characters_all_of_piece() {
+        // Given
+        let input = String::from("This is a text.");
+        let mut piece_table = PieceTable::new(input);
+
+        // When
+        piece_table = piece_table.remove(0, 15);
+
+        // Then
+        let text = piece_table.get_text();
+        assert_eq!(text, String::from(""));
+    }
+
+    #[test]
+    fn remove_when_index_is_length_of_text_should_remove_characters_within_of_piece() {
+        // Given
+        let input = String::from("This isxxxx a text.");
+        let mut piece_table = PieceTable::new(input);
+
+        // When
+        piece_table = piece_table.remove(7, 4);
 
         // Then
         let text = piece_table.get_text();
